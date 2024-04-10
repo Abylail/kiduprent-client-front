fd1f0e834717329db2ec57b1d489f1b80cbc1506
Абылай <bazarbayev.abylay@gmail.com>
2024-04-10 22:27:35 +0500

    thank tou

--- components/common/main/prices.vue
+++ components/common/main/prices.vue
@@ -50,7 +50,7 @@
         <base-icon class="thanks-window__icon" name="mdi-party-popper" size="70"/>
         <div class="thanks-window__title">{{ $t('thank_you_for_request') }}</div>
         <div class="thanks-window__subtitle">{{ $t('manager_calls_you') }}</div>
-        <base-button type="yellow" size="big" full-width @click="router.push('/main')">{{ $t('go_main') }}</base-button>
+        <base-button type="yellow" size="big" full-width @click="thanksWindow = false">{{ $t('go_main') }}</base-button>
       </div>
     </base-backdrop>
   </div>
